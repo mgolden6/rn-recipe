2697b91b5bde061fd7437962e71aae521042734a
Michael <mgolden6@gmail.com>
2020-08-25 00:26:41 -0400

    06_116_navigate_between_screens

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const CategoriesScreen = () => {
+const CategoriesScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Categories Screen</Text>
+      <Button
+        title="Go To Meals"
+        onPress={() => {
+          props.navigation.navigate("CategoryMeals");
+        }}
+      />
     </View>
   );
 };
