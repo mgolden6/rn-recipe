9728dc96b69de11ea3dcb2e695460affcc6a59f1
Michael <mgolden6@gmail.com>
2020-09-02 22:16:08 -0400

    06_133_configure_tabs_with_icons

--- navigation/MealsNavigation.jsx
+++ navigation/MealsNavigation.jsx
@@ -1,14 +1,17 @@
+import React from "react";
+import { Platform } from "react-native";
+
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
 
-import { Platform } from "react-native";
 import Theme from "../constants/Theme";
-import { createBottomTabNavigator } from "react-navigation-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const MealsNavigator = createStackNavigator(
   {
@@ -33,9 +36,42 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  Favorites: FavoritesScreen,
-});
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <MaterialIcons
+              name="restaurant"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: FavoritesScreen,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <MaterialIcons
+              name="favorite"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Theme.colors.accentColor,
+    },
+  }
+);
 
 export default createAppContainer(MealsFavTabNavigator);
