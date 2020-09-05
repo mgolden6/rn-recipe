6d1ce246a6615fe7dcfda70ec613f7494861d550
Michael <mgolden6@gmail.com>
2020-09-05 17:13:57 -0400

    06_143_passing_data_between_component_and_navigation_options_header

--- screens/FiltersScreen.jsx
+++ screens/FiltersScreen.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { View, Text, StyleSheet, Platform } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import { Switch } from "react-native-gesture-handler";
 import { DrawerActions } from "react-navigation-drawer";
 
 import Theme from "../constants/Theme";
 import CustomHeaderButton from "../components/CustomHeaderButton";
 import FilterSwitch from "../components/FilterSwitch";
 
-const FiltersScreen = () => {
+const FiltersScreen = (props) => {
+  const { navigation } = props;
+
   const [isGlutenFree, setIsGlutenFree] = useState(false);
   const [isLactoseFree, setIsLactoseFree] = useState(false);
   const [isVegan, setIsVegan] = useState(false);
   const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const saveFilters = useCallback(() => {
+    const appliedFilters = {
+      glutenFree: isGlutenFree,
+      lactoseFree: isLactoseFree,
+      vegan: isVegan,
+      vegetarian: isVegetarian,
+    };
+    console.log(appliedFilters);
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+  useEffect(() => {
+    navigation.setParams({ save: saveFilters });
+  }, [saveFilters]);
 
   return (
     <View style={styles.screen}>
@@ -52,6 +67,15 @@ FiltersScreen.navigationOptions = (navData) => {
           onPress={() => {
             navData.navigation.dispatch(DrawerActions.toggleDrawer());
           }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Save"
+          iconName="save"
+          onPress={navData.navigation.getParam("save")}
         />
       </HeaderButtons>
     ),
