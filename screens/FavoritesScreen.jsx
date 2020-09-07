09f0837931cfdaa75905c2790c1ca1104a4c81bd
Michael <mgolden6@gmail.com>
2020-09-06 21:50:25 -0400

    07_154_redering_favorites_fallback_text

--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -5,10 +5,20 @@ import CustomHeaderButton from "../components/CustomHeaderButton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { DrawerActions } from "react-navigation-drawer";
 import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favoriteMeals);
-  return <MealList listData={favMeals} navigation={props.navigation} />;
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.noFavsText}>
+        <DefaultText>No favorite Meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  } else {
+    return <MealList listData={favMeals} navigation={props.navigation} />;
+  }
 };
 
 FavoritesScreen.navigationOptions = (navData) => {
@@ -27,5 +37,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  noFavsText: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default FavoritesScreen;
